a39a9c873855659ce01052d7692df50ec27e27b1
Enzo Naso <enzo@naso-fam.ch>
2015-04-21 08:18:10 +0200

    4. Tutorial Video zu java

--- readme.txt
+++ readme.txt
@@ -1,1 +1,3 @@
 Videos von Youtuber "Hendrik Schmidt"
+
+https://www.youtube.com/watch?v=5GPtviB6qDE&list=PLdRbhbTUg6E6yb8dp7ty2Uw3FggKDDgjT&index=1
---
+++ video 4/SpezielleOperatoren.java
@@ -0,0 +1,19 @@
+public class SpezielleOperatoren
+{
+	public static void main(String[] args){
+		
+		//Variablendeklaration
+		int i = 0;
+		
+		//Spezielle arithmetsiche Operatoren
+		i += 3;		// i = 3
+		i -= 1;		// i = 2
+		i *= 3;		// i = 6
+		i /= 2;		// i = 3
+		i %= 2;		// i = 1
+		
+		//Ausgabe
+		System.out.println(i);
+	
+	}
+}
