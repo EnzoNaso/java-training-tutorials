ff0de870d64273d41f06077594ed6c2e045c2292
Vincenzo Naso <enzo@naso-fam.ch>
2015-05-02 23:45:03 +0200

    6. Tutorial Video zu java

---
+++ video 6/ArraysErstellen.java
@@ -0,0 +1,19 @@
+import java.util.*; 
+
+public class ArraysErstellen{
+    public static void main(String[] args){
+        //Array erstellen 
+        String[] namen = new String[3];
+
+        //Strings in Array abspeichern
+        namen[0] = "Peter";
+        namen[1] = "Hans";
+        namen[2] = "Klaus";
+
+        //Ausgabe
+        System.out.println(Arrays.toString(namen));
+
+    
+    }
+
+}
